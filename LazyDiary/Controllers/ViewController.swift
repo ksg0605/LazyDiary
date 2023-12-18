0a07dbd3d652f757ce72b765f460f4cf6cde3cfb
ksg0605 <ksg0605@gmail.com>
2023-12-18 19:58:53 +0900

    [Add] Mapkit

--- LazyDiary/Controllers/ViewController.swift
+++ LazyDiary/Controllers/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    
+    let mapView: MKMapView = {
+       let map = MKMapView()
+        // 테마 설정
+        map.overrideUserInterfaceStyle = .dark
+        map.preferredConfiguration.elevationStyle = .flat
+        map.setCenter(<#T##coordinate: CLLocationCoordinate2D##CLLocationCoordinate2D#>, animated: <#T##Bool#>)
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        setupNavigationBar()
+        setMapConstraints()
     }
 
+    private func setupNavigationBar() {
+        self.title = "지도"
+    }
+    
+    func setMapConstraints() {
+        view.addSubview(mapView)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        
+    }
 
 }
-
