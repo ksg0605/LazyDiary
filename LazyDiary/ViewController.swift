ffd6f28908184c631d53e4b4e95c7a03fcf8ca95
ksg0605 <ksg0605@gmail.com>
2023-12-18 18:02:42 +0900

    Initial Commit

---
+++ LazyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyDiary
+//
+//  Created by 김선규 on 12/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
