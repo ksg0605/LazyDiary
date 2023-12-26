8b3277d1984bb2803ca7554024b6b5455c94eb98
ksg0605 <ksg0605@gmail.com>
2023-12-26 21:38:34 +0900

    [Add] NaverMapAPI / DetailView

---
+++ LazyDiary/Models/NaverMap.swift
@@ -0,0 +1,12 @@
+//
+//  NaverMap.swift
+//  LazyDiary
+//
+//  Created by 김선규 on 12/26/23.
+//
+
+import Foundation
+
+class NaverMap {
+    
+}
---
+++ LazyDiary/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  LazyDiary
+//
+//  Created by 김선규 on 12/26/23.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
